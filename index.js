0df0bc2cac219aed32925af94f8c827c2ae2e904
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-07-18 22:41:29 +0530

    comments added

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_r53Hu3Rb3HE5CrLlywAAia5p2VvxreIvwLJNjqAZ');
 
+// Money Converter 👍 
 
 export async function moneyConverter(fromMoney, toMoney, units) {
-
     const res = await freecurrencyapi.latest({
         base_currency: fromMoney,
         currencies: toMoney
@@ -14,8 +14,5 @@ export async function moneyConverter(fromMoney, toMoney, units) {
     const multiplier = res.data[toMoney];
     return multiplier * units;
 }
-
-
-
 
 
